593adada2b8ffa67a94de9fdf8d404dcbaaca5f8
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-08-17 21:59:14 +0530

    first commit

---
+++ rel_ops.java
@@ -0,0 +1,14 @@
+public class rel_ops {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 12;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        
+
+    }
+}
---
+++ stri.java
@@ -0,0 +1,7 @@
+public class stri {
+    public static void main(String[] args) {
+        String name = "Aaryan";
+        System.out.println(name);
+
+    }
+}
