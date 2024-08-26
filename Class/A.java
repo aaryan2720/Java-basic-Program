beef671256e5d27105dfec63dfb9ea768cd5e405
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-08-26 13:33:51 +0530

    Class program

---
+++ Class/A.java
@@ -0,0 +1,13 @@
+class A{
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.display();
+    }
+    
+}
+
+class B{
+    public void display() {
+        System.out.println("Hi john doe");
+    }
+}
---
+++ Class/Studentmain.java
@@ -0,0 +1,18 @@
+public class Studentmain {
+    int a;
+    String n;
+    public static void main(String[] args) {
+        Studentmain s = new Studentmain();
+        s.setData("Aaryan Choudhari",19);
+        s.displayData();
+    }
+    public void setData(String name,int age){
+        n=name;
+        a=age;
+        }
+        public void displayData(){
+            System.out.println("Name: "+n);
+            System.out.println("Age: "+a);
+            }
+
+}
