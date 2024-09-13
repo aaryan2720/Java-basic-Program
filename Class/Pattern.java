da1ce5a3cb183e02c0b31b90ddc6c5c54dda99d5
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-09-13 21:42:17 +0530

    Pattern added

---
+++ Class/Pattern.java
@@ -0,0 +1,10 @@
+class Pattern {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++) {
+            for(j=i;j<=i;j--) {
+                System.out.print("\n*");
+            }        
+        }
+    }
+}
