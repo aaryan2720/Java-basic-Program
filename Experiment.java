fbb28c09acecfcac4bfc99184ad41b2446209ce2
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-11-06 14:59:32 +0530

    "Removed empty lines and condensed class definitions"

--- Experiment.java
+++ Experiment.java
@@ -67,10 +67,7 @@ class Result extends Test implements Sports {
     float calculateFinalResult() {
         return getTest1() + getTest2() + getSportsMarks();
     }
-}
 
-
-class Execution {
     public static void main(String[] args) {
         Result studentResult = new Result(42, "Aaryan", 80.00f, 90.0f);
         System.out.println("Roll No: " + studentResult.getRollNo());
