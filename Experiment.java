e52ee77239af6b93ac70de3d1966952e2cb6a87a
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-12-02 09:10:21 +0530

    "Added two blank lines in Result class"

--- Experiment.java
+++ Experiment.java
@@ -65,6 +65,8 @@ class Result extends Test implements Sports {
         return SPORTS_MARKS;
     }
 
+    
+
     float calculateFinalResult() {
         return getTest1() + getTest2() + getSportsMarks();
     }
