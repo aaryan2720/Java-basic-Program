a64ff299e8980bc895de243f450d21dc41945793
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-12-25 21:32:40 +0530

    "Removed unused code and main method from Result class."

--- Experiment.java
+++ Experiment.java
@@ -69,7 +69,6 @@ class Result extends Test implements Sports {
         return getTest1() + getTest2() + getSportsMarks();
     }
     
-    
     public static void main(String[] args) {
         Result studentResult = new Result(42, "Aaryan", 80.00f, 90.0f);
         System.out.println("Roll No: " + studentResult.getRollNo());
